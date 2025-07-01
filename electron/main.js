1966ab440af15f747fcc1c66b32ffb429d77a8ee
ivett-ztm <sharisgomez9@gmail.com>
2025-07-01 10:32:18 -0600

    electron

---
+++ electron/main.js
@@ -0,0 +1,25 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), // si usas preload
+    },
+  });
+
+  // Carga la URL del servidor de Vite (en desarrollo)
+  win.loadURL('http://localhost:5173');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
